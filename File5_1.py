7da20c7d7236366b408d163d14b281a673f14dc4
UstNana <41002698+UstNana@users.noreply.github.com>
2018-09-12 11:11:31 +0300

    Add File5_1.py

--- File5_1.py
+++ File5_1.py
@@ -1,15 +1,21 @@
+# -*- coding: utf-8 -*-
+#file = open("txt.txt")
+#file_py = file.read().split("\n")[:-1]
 
-file = open("file.txt")
-file1 = file.read().split("\n")[:-1]
 
-dict = dict()
+dict_2 = {}
+dict_1 = {}
+with open("txt.txt") as file:
+    for i, line in enumerate(file):
+        if i == 1:
+              for headline in file:
+                key_1 = headline.split(" ")[0]
+                dict_1[key_1] = dict_2
+        if i == 2:
+            for headline in file:
+                key = headline.split("")[0]
+                value = headline.split(" ")[1:]
+                dict_2[key] = value
 
-for line in file1:
-    key = line.split(" ")[0]
-    key = dict()
-    for element in line:
-      key1 = line.split(" ")[0]
-      value = line.split(" ")[1:]
-      dict[key1] = value
-
+print(dict_1)
 file.close()
